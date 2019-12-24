f06cd84b7cb2c462ad3a6757e967d05f05f54360
kbys-t <kobayashi@is.naist.jp>
2019-12-24 17:54:33 +0900

    add __init__

---
+++ torchoptim/optimizers/__init__.py
@@ -0,0 +1,9 @@
+from torchoptim.optimizers.TAdam import TAdam
+
+from torchoptim.optimizers.RoAdam import RoAdam
+
+from torchoptim.optimizers.AdaBound import AdaBound
+from torchoptim.optimizers.TAdaBound import TAdaBound
+
+from torchoptim.optimizers.RAdam import RAdam
+from torchoptim.optimizers.TRAdam import TRAdam
---
+++ torchoptim/regularizers/__init__.py
@@ -0,0 +1,3 @@
+from torchoptim.regularizers.L2 import L2
+from torchoptim.regularizers.EWC import EWC
+from torchoptim.regularizers.EWCS import EWCS
