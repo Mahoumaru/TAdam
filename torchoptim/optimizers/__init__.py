37025250298053627eee415e4583680d26df192a
kbys-t <kobayashi@is.naist.jp>
2020-01-16 09:38:33 +0900

    modify __init__

--- torchoptim/optimizers/__init__.py
+++ torchoptim/optimizers/__init__.py
@@ -1,9 +1,5 @@
 from torchoptim.optimizers.TAdam import TAdam
+from torchoptim.optimizers.TAdaBound import TAdaBound
+from torchoptim.optimizers.TRAdam import TRAdam
 
 from torchoptim.optimizers.RoAdam import RoAdam
-
-from torchoptim.optimizers.AdaBound import AdaBound
-from torchoptim.optimizers.TAdaBound import TAdaBound
-
-from torchoptim.optimizers.RAdam import RAdam
-from torchoptim.optimizers.TRAdam import TRAdam
