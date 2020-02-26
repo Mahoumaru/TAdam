b4922e3ff4ac75369762d11f92978464192fe528
Leomaru <33780483+Mahoumaru@users.noreply.github.com>
2020-02-26 12:17:19 +0900

    Update launcher_adab.py

--- Demos/Classification_demo/launcher_adab.py
+++ Demos/Classification_demo/launcher_adab.py
@@ -7,20 +7,20 @@ os.system("echo " + start_signal)
 
 os.system("echo Adam")
 os.system("python main.py --model=resnet --optim=adam --lr=0.001 --beta1=0.99 --beta2=0.999")
-os.system("python plot_originalversion.py -p Adam")
+os.system("python plot.py -p Adam")
 
 os.system("echo TAdam")
 os.system("python main.py --model=resnet --optim=tadam --lr=0.001 --beta1=0.99 --beta2=0.999")
-os.system("python plot_originalversion.py -p Adam TAdam")
+os.system("python plot.py -p Adam TAdam")
 
 os.system("echo AMSGrad")
 os.system("python main.py --model=resnet --optim=amsgrad --lr=0.001 --beta1=0.99 --beta2=0.999")
-os.system("python plot_originalversion.py -p Adam TAdam AMSGrad")
+os.system("python plot.py -p Adam TAdam AMSGrad")
 
 os.system("echo TAMSGrad")
 os.system("python main.py --model=resnet --optim=tamsgrad --lr=0.001 --beta1=0.99 --beta2=0.999")
-os.system("python plot_originalversion.py -p Adam TAdam AMSGrad TAMSGrad")
+os.system("python plot.py -p Adam TAdam AMSGrad TAMSGrad")
 
 os.system("echo RoAdam")
 os.system(" python main.py --model=resnet --optim=roadam --lr=0.001 --beta1=0.99 --beta2=0.999")
-os.system("python plot_originalversion.py -p Adam TAdam AdaBound RoAdam AMSGrad TAMSGrad")
+os.system("python plot.py -p Adam TAdam AdaBound RoAdam AMSGrad TAMSGrad")
