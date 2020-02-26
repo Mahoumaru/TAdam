53330396353758495b75b5945dded6acffa3e10c
Leomaru <33780483+Mahoumaru@users.noreply.github.com>
2020-02-26 12:16:19 +0900

    Update launcher_adab.py

--- Demos/Classification_demo/launcher_adab.py
+++ Demos/Classification_demo/launcher_adab.py
@@ -24,23 +24,3 @@ os.system("python plot_originalversion.py -p Adam TAdam AMSGrad TAMSGrad")
 os.system("echo RoAdam")
 os.system(" python main.py --model=resnet --optim=roadam --lr=0.001 --beta1=0.99 --beta2=0.999")
 os.system("python plot_originalversion.py -p Adam TAdam AdaBound RoAdam AMSGrad TAMSGrad")
-
-"""os.system("echo AdaBound")
-os.system("python main.py --model=resnet --optim=adabound --lr=0.001 --final_lr=0.1")
-os.system("python plot_originalversion.py -p Adam TAdam AMSGrad TAMSGrad AdaBound")
-
-os.system("echo TAdaBound")
-os.system("python3 main.py --model=resnet --optim=tadabound --lr=0.001 --final_lr=0.1")
-os.system("python3 plot.py -p Adam TAdam AdaBound TAdaBound")
-
-os.system("echo AMSBound")
-os.system("python3 main.py --model=resnet --optim=amsbound --lr=0.001 --final_lr=0.1")
-os.system("python3 plot.py -p Adam TAdam AdaBound TAdaBound AMSGrad TAMSGrad AMSBound")
-
-os.system("echo TAMSBound")
-os.system("python3 main.py --model=resnet --optim=tamsbound --lr=0.001 --final_lr=0.1")
-os.system("python3 plot.py -p Adam TAdam AdaBound TAdaBound AMSGrad TAMSGrad AMSBound TAMSBound")"""
-
-"""os.system("echo SGD")
-os.system("python3 main.py")
-os.system("python3 plot.py -p Adam TAdam AdaBound TAdaBound AMSGrad TAMSGrad AMSBound TAMSBound SGD")"""
