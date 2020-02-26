e0dd842a877f692724a24d1b44ad6f070959a057
Leomaru <33780483+Mahoumaru@users.noreply.github.com>
2020-02-26 11:30:29 +0900

    Update __init__.py

--- torchoptim/optimizers/__init__.py
+++ torchoptim/optimizers/__init__.py
@@ -1,5 +1,3 @@
 from torchoptim.optimizers.TAdam import TAdam
-from torchoptim.optimizers.TAdaBound import TAdaBound
-from torchoptim.optimizers.TRAdam import TRAdam
 
 from torchoptim.optimizers.RoAdam import RoAdam
